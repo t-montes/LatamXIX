9f9b3cac146a9ee09f9e09c9e73b10ac1eb84723
Tony <santiago.montesb@gmail.com>
2024-04-28 19:20:12 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# LatamXIX
+## Spanish Newspaper Corpus from 19th Century Latin America with LLM OCR Correction
+
+
+
---
+++ structuring.py
@@ -0,0 +1,36 @@
+import os
+import json
+import pandas as pd
+
+def extract_data_from_json(json_file):
+    with open(json_file, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+        metadata = data['metadata']
+        text_contexts = data['contexts']
+        rows = []
+        for context in text_contexts:
+            row = {
+                'newspaper_id': metadata['id'],
+                'text_id': f"{metadata['file']}-{metadata['page']}-{context['id']}",
+                'title': metadata['newspaper'],
+                'year': metadata['year'],
+                'city': metadata['city'],
+                'text': context['text']
+            }
+            rows.append(row)
+        return rows
+
+def process_folder(folder_path):
+    all_data = []
+    for root, _, files in os.walk(folder_path):
+        for file in files:
+            if file.endswith('.json'):
+                json_file = os.path.join(root, file)
+                rows = extract_data_from_json(json_file)
+                all_data.extend(rows)
+    return pd.DataFrame(all_data).astype({'year': 'str'})
+
+folder_path = './data/raw'
+df_latamnp = process_folder(folder_path)
+df_latamnp.to_parquet('./data/original-latam-xix.parquet')
+#df_latamnp.to_csv('./data/original-latam-xix.tsv', sep='\t', index=False)
